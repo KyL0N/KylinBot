dcab1c294502475ef26bcaa3ba0733e986f5203c
Kylin_zrz <y13308074856@gmail.com>
2021-11-26 23:23:27 +0800

    feat: change the protocol from default to PAD

---
+++ src/main/java/top/kylinbot/demo/KylinBotApplication.java
@@ -0,0 +1,31 @@
+package top.kylinbot.demo;
+
+import love.forte.common.configuration.Configuration;
+import love.forte.simbot.annotation.SimbotApplication;
+import love.forte.simbot.api.sender.BotSender;
+import love.forte.simbot.bot.Bot;
+import love.forte.simbot.core.SimbotApp;
+import love.forte.simbot.core.SimbotContext;
+import love.forte.simbot.core.SimbotProcess;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDateTime;
+
+@SimbotApplication
+public class KylinBotApplication implements SimbotProcess {
+    public static void main(String[] args) {
+        SimbotApp.run(KylinBotApplication.class, args);
+    }
+
+    @Override
+    public void post(@NotNull SimbotContext context) {
+        Bot bot = context.getBotManager().getDefaultBot();
+        BotSender sender = bot.getSender();
+        sender.SENDER.sendPrivateMsg(1579525246, "Bot is started\n" + LocalDateTime.now());
+    }
+
+    @Override
+    public void pre(@NotNull Configuration config) {
+
+    }
+}
