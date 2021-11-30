6343d9514f60d5fde0e097bace1aa0cd4785c048
Kylin_zrz <y13308074856@gmail.com>
2021-11-30 11:43:32 +0800

    feat: accept all add friend request

---
+++ src/main/java/top/kylinbot/demo/listener/FriendAddListener.java
@@ -0,0 +1,34 @@
+package top.kylinbot.demo.listener;
+
+import love.forte.common.ioc.annotation.Beans;
+import love.forte.simbot.annotation.OnFriendAddRequest;
+
+import love.forte.simbot.annotation.OnFriendIncrease;
+import love.forte.simbot.api.message.assists.Flag;
+import love.forte.simbot.api.message.assists.FlagContent;
+import love.forte.simbot.api.message.containers.AccountInfo;
+import love.forte.simbot.api.message.containers.BotInfo;
+import love.forte.simbot.api.message.events.FriendAddRequest;
+import love.forte.simbot.api.message.events.FriendIncrease;
+import love.forte.simbot.api.message.events.RequestGet;
+import love.forte.simbot.api.sender.Sender;
+import love.forte.simbot.api.sender.Setter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@Beans
+public class FriendAddListener {
+
+    @OnFriendAddRequest
+    public void acceptFriendRequest(FriendAddRequest flag, Setter setter) {
+        setter.acceptFriendAddRequest(flag.getFlag());
+    }
+
+    @OnFriendIncrease
+    public void welcomeNewFriend(FriendIncrease friendIncrease, Sender sender) {
+        sender.sendPrivateMsg(friendIncrease.getAccountInfo().getAccountCode(), "hello im Asagodness");
+    }
+
+
+
+}
