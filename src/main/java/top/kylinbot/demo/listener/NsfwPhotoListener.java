404af5ab8ca04e595b760ec32bbdd852ec42fc95
Kylin_zrz <y13308074856@gmail.com>
2021-12-05 20:31:09 +0800

    feat: add more api to nsfw listener

--- src/main/java/top/kylinbot/demo/listener/NsfwPhotoListener.java
+++ src/main/java/top/kylinbot/demo/listener/NsfwPhotoListener.java
@@ -1,6 +1,7 @@
 package top.kylinbot.demo.listener;
 
 import catcode.CatCodeUtil;
+import catcode.CatEncoder;
 import love.forte.common.ioc.annotation.Beans;
 import love.forte.simbot.annotation.Filter;
 import love.forte.simbot.annotation.OnGroup;
@@ -10,8 +11,10 @@ import love.forte.simbot.api.message.events.PrivateMsg;
 import love.forte.simbot.api.sender.MsgSender;
 import love.forte.simbot.filter.MatchType;
 import top.kylinbot.demo.service.NsfwService;
+import top.kylinbot.demo.util.DeflaterUtil;
 
 import java.io.IOException;
+import java.util.Random;
 
 @Beans
 public class NsfwPhotoListener extends NsfwService {
@@ -20,6 +23,7 @@ public class NsfwPhotoListener extends NsfwService {
 
     /**
      * 获取R18标志, 0为off 1为on
+     *
      * @return 返回r18值
      */
     public static int getR18() {
@@ -30,30 +34,116 @@ public class NsfwPhotoListener extends NsfwService {
     @Filter(value = "show me the photo", trim = true, matchType = MatchType.STARTS_WITH)
     public void sendImg(GroupMsg msg, MsgSender sender) {
         CatCodeUtil util = CatCodeUtil.INSTANCE;
-        String image = util.getStringTemplate().image(IMG_PATH);
+        String image = util.getStringTemplate().image("https://www.yingciyuan.cn/api.php");
         sender.SENDER.sendGroupMsg(msg.getGroupInfo().getGroupCode(), image);
     }
 
 
     @OnGroup
-    @Filter(value = ".uestc", trim = true, matchType = MatchType.STARTS_WITH)
+    @Filter(value = ".uestc", matchType = MatchType.EQUALS)
     public void sendGroupPixivPic(GroupMsg groupMsg, MsgSender sender) throws IOException {
 //        sender.SENDER.sendGroupMsg(groupMsg.getGroupInfo().getGroupCode(), "url:" + urls);
-        if (getR18() == 1){
+        if (getR18() == 1) {
             sender.SENDER.sendGroupMsg(groupMsg.getGroupInfo().getGroupCode(), "R18功能现对群聊关闭");
         }
         String catCode1 = getCatCodeFromApi(false);
+        int ziptime = 3;
+        while (ziptime > 0) {
+            catCode1 = DeflaterUtil.zipString(catCode1);
+            catCode1 = DeflaterUtil.unzipString(catCode1);
+            ziptime -= 1;
+        }
         sender.SENDER.sendGroupMsg(groupMsg.getGroupInfo().getGroupCode(), catCode1);
 
     }
 
-
     @OnPrivate
-    @Filter(value = ".uestc", trim = true, matchType = MatchType.STARTS_WITH)
+    @Filter(value = ".uestc", matchType = MatchType.EQUALS)
     public void sendPrivatePixivPic(PrivateMsg privateMsg, MsgSender sender) throws IOException {
 //        sender.SENDER.sendPrivateMsg(privateMsg.getAccountInfo().getAccountCode(), "url:" + urls);
         String catCode1 = getCatCodeFromApi(true);
+        int ziptime = 3;
+        while (ziptime > 0) {
+            catCode1 = DeflaterUtil.zipString(catCode1);
+            catCode1 = DeflaterUtil.unzipString(catCode1);
+            ziptime -= 1;
+        }
         sender.SENDER.sendPrivateMsg(privateMsg.getAccountInfo().getAccountCode(), catCode1);
+    }
+
+    @OnPrivate
+    @Filter(value = ".zju", matchType = MatchType.EQUALS)
+    public void sendPrivateTvaPic(PrivateMsg privateMsg, MsgSender sender) throws IOException {
+//        sender.SENDER.sendPrivateMsg(privateMsg.getAccountInfo().getAccountCode(), "url:" + urls);
+        String catCode1 = getPic(true);
+        int ziptime = 3;
+        while (ziptime > 0) {
+            catCode1 = DeflaterUtil.zipString(catCode1);
+            catCode1 = DeflaterUtil.unzipString(catCode1);
+            ziptime -= 1;
+        }
+        sender.SENDER.sendPrivateMsg(privateMsg.getAccountInfo().getAccountCode(), catCode1);
+    }
+
+    @OnGroup
+    @Filter(value = ".zju", matchType = MatchType.EQUALS)
+    public void sendGroupTvaPic(GroupMsg groupMsg, MsgSender sender) throws IOException {
+//        sender.SENDER.sendGroupMsg(groupMsg.getGroupInfo().getGroupCode(), "url:" + urls);
+        String catCode1 = getPic(false);
+        int ziptime = 3;
+        while (ziptime > 0) {
+            catCode1 = DeflaterUtil.zipString(catCode1);
+            catCode1 = DeflaterUtil.unzipString(catCode1);
+            ziptime -= 1;
+        }
+        sender.SENDER.sendGroupMsg(groupMsg.getGroupInfo().getGroupCode(), catCode1);
+
+    }
+
+    @OnGroup
+    @Filter(value = ".thu", matchType = MatchType.STARTS_WITH)
+    public void sendAcgImg(GroupMsg msg, MsgSender sender) {
+        CatCodeUtil util = CatCodeUtil.INSTANCE;
+        String url;
+        String url1 = "https://iw233.cn/API/Random.php";
+        String url2 = "https://iw233.cn/API/Ghs.php";
+        String url3 = "https://www.yingciyuan.cn/api.php";
+        int seed = (int) (Math.random() * 30);
+        if (seed < 10) {
+            url = url1;
+            System.out.println(seed + "\n");
+        } else if (seed < 20) {
+            url = url2;
+            System.out.println(seed + "\n");
+        } else {
+            url = url3;
+            System.out.println(seed + "\n");
+        }
+        String image = util.getStringTemplate().image(url);
+        sender.SENDER.sendGroupMsg(msg.getGroupInfo().getGroupCode(), image);
+    }
+
+    @OnPrivate
+    @Filter(value = ".thu", matchType = MatchType.STARTS_WITH)
+    public void sendAcgImg(PrivateMsg msg, MsgSender sender) {
+        CatCodeUtil util = CatCodeUtil.INSTANCE;
+        String url;
+        String url1 = "https://iw233.cn/API/Random.php";
+        String url2 = "https://iw233.cn/API/Ghs.php";
+        String url3 = "https://www.yingciyuan.cn/api.php";
+        int seed = (int) (Math.random() * 30);
+        if (seed < 10) {
+            url = url1;
+            System.out.println("1\n");
+        } else if (seed < 20) {
+            url = url2;
+            System.out.println("2\n");
+        } else {
+            url = url3;
+            System.out.println("3\n");
+        }
+        String image = util.getStringTemplate().image(url);
+        sender.SENDER.sendPrivateMsg(msg.getAccountInfo().getAccountCode(), image);
     }
 
 
--- src/main/java/top/kylinbot/demo/service/NsfwService.java
+++ src/main/java/top/kylinbot/demo/service/NsfwService.java
@@ -17,10 +17,19 @@ import static top.kylinbot.demo.listener.NsfwPhotoListener.getR18;
 
 
 public class NsfwService {
+    private static final String iw233 = "https://iw233.cn/API/Random.php";
+    private static final String toubiec = "https://acg.toubiec.cn/random.php?ret=json";
+    //    original
+//    regular
+//    small
+//    thumb
+//    mini
+    private final String size = "regular";
 
     public String getCatCodeFromApi(Boolean isPri) throws IOException {
         HttpClient client = HttpClients.createDefault();
-        String url = "https://api.lolicon.app/setu/v2?size=regular";
+//        String url = "https://api.lolicon.app/setu/v2?size=regular";
+        String url = "https://api.lolicon.app/setu/v2?size=" + size;
         if (isPri) {
             if (getR18() == 1) {
                 url = url + "&r18=1";
@@ -39,6 +48,32 @@ public class NsfwService {
                 return "由于腾讯风险控制, 链接请自行查看: \n" + urls;
             }
         }
+
+        return util.getStringTemplate().image(urls);
+    }
+
+
+    public String getPic(Boolean isPri) throws IOException {
+        HttpClient client = HttpClients.createDefault();
+        String url = toubiec;
+        if (isPri) {
+            if (getR18() == 1) {
+                url = url + "&r18=1";
+            }
+        }
+        url = url + "&r18=0";
+        HttpGet get = new HttpGet(url);
+        HttpResponse response = client.execute(get);
+        HttpEntity entity = response.getEntity();
+        String string = EntityUtils.toString(entity);
+        String urls = parseToubiecJson(string);
+//        String urlEncoded = CatEncoder.getInstance().encodeParams(urls);
+        CatCodeUtil util = CatCodeUtil.INSTANCE;
+        if (isPri) {
+            if (getR18() == 1) {
+                return "由于腾讯风险控制, 链接请自行查看: \n" + urls;
+            }
+        }
         return util.getStringTemplate().image(urls);
     }
 
@@ -46,12 +81,25 @@ public class NsfwService {
      * @param string 传入get到的Json信息
      * @return 返回解析到的图片url信息
      */
+    public String parseToubiecJson(String string) {
+        JSONArray data = JSON.parseArray(string);
+//        JSONArray data = jsonObject.getJSONArray(0);
+//        String r18 = data.getJSONObject(0).getString("r18");
+//        String uid = data.getJSONObject(0).getString("uid");
+        String url = data.getJSONObject(0).getString("imgurl");
+
+        System.out.println("url:" + url);
+//        System.out.println("r18:" + r18);
+        //i.pixiv.cat现以被墙, i.pixiv.re可正常使用
+        return url;
+    }
+
     public String parseJson(String string) {
         JSONObject jsonObject = JSON.parseObject(string);
         JSONArray data = jsonObject.getJSONArray("data");
         String r18 = data.getJSONObject(0).getString("r18");
         String uid = data.getJSONObject(0).getString("uid");
-        String url = data.getJSONObject(0).getJSONObject("urls").getString("regular");
+        String url = data.getJSONObject(0).getJSONObject("urls").getString(size);
 //        String pid = data.getJSONObject(0).getString("pid");
 //        String title = data.getJSONObject(0).getString("title");
 //        String author = data.getJSONObject(0).getString("author");
