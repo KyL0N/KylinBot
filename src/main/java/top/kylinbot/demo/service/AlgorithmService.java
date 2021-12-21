df6cb5489fd46b8b317d4922fd29215e0855b8fd
Kylin_zrz <y13308074856@gmail.com>
2021-12-21 10:49:53 +0800

    fix: fix bracketCalculate in algorithm

--- src/main/java/top/kylinbot/demo/service/AlgorithmService.java
+++ src/main/java/top/kylinbot/demo/service/AlgorithmService.java
@@ -10,20 +10,37 @@ public class AlgorithmService {
 
     public int bracketCalculate(String str) {
         Stack<Character> stk = new Stack<>();
+        Stack<Integer> ansStk = new Stack<>();
+
         int i = 0, ans = 0;
         while (i < str.length()) {
             Character chr = str.charAt(i);
-            if (chr.equals('('))
+            if (chr.equals('(')) {
                 stk.push(chr);
-            else if (chr.equals(')')) {
-                Character c = stk.pop();
-                if (c.equals('('))
+            } else if (chr.equals(')')) {
+                Character c;
+                try {
+                    c = stk.peek();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return -1;
+                }
+
+                if (c != null && c.equals('(')) {
                     ans++;
-                else if (c.equals(')'))
+                    if (i + 2 < str.length() && str.charAt(i + 2) == '(') {
+                        ansStk.push(ans);
+                        ans = 0;
+                    }
+                } else if (c != null && c.equals(')')) {
                     ans = ans * 2;
+                }
                 stk.push(')');
             }
             i++;
+        }
+        while (!ansStk.isEmpty()) {
+            ans += ansStk.pop();
         }
         return ans;
     }
